a860d471b20c651c04506b59269bbdd807e21a29
saxonmahar <maharsaxon2058@gmail.com>
2026-02-03 17:31:37 +0545

    Export schedule API in services index

--- frontend/src/services/api/index.js
+++ frontend/src/services/api/index.js
@@ -3,4 +3,5 @@ export { authService } from './auth';
 export { userService } from './user';
 export { yogaService } from './yoga';
 export { dietService } from './diet';
+export { scheduleAPI } from './schedule';
 export { default as apiClient } from './client';
